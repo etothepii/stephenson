4a7933df4462048d771fd89c48302a9e86986366
James Robinson <github@epii.co.uk>
2014-02-10 09:29:55 +0000

    Made pom file build working application

---
+++ src/main/java/uk/co/epii/conservatives/georgecanning/Main.java
@@ -0,0 +1,9 @@
+package uk.co.epii.conservatives.georgecanning;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Running George Canning");
+  }
+
+}
---
+++ src/test/java/uk/co/epii/conservatives/georgecanning/MainTest.java
@@ -0,0 +1,14 @@
+package uk.co.epii.conservatives.georgecanning;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+  
+  @Test
+  public void simpleTest() {
+    assertEquals("Basic arithmetic", 2, 1 + 1);
+  }
+
+}
