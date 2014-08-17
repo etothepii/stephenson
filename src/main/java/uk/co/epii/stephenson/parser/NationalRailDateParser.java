f122662f8929989ea968946dcde55d4a2d2ad351
James Robinson <github@epii.co.uk>
2014-08-17 12:37:54 +0100

    Added code to NationalRailDateParser passes tests

--- src/main/java/uk/co/epii/stephenson/parser/NationalRailDateParser.java
+++ src/main/java/uk/co/epii/stephenson/parser/NationalRailDateParser.java
@@ -12,6 +12,18 @@ public class NationalRailDateParser implements Parser<Date> {
 
   @Override
   public Date parse(String string) {
-    return null;
+    if (string.equals("999999")) {
+      return null;
+    }
+    int year = Integer.parseInt(string.substring(0,2));
+    int month = Integer.parseInt(string.substring(2,4));
+    int day = Integer.parseInt(string.substring(4,6));
+    if (year < 60) {
+      year += 2000;
+    }
+    else {
+      year += 1900;
+    }
+    return new Date(year, month, day);
   }
 }
