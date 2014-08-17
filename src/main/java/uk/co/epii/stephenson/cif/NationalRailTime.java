c2e1fd960b728bb62c15b898fc4269b982e1f674
James Robinson <github@epii.co.uk>
2014-08-17 14:21:23 +0100

    Made an interface for representing national rail time@

---
+++ src/main/java/uk/co/epii/stephenson/cif/NationalRailTime.java
@@ -0,0 +1,15 @@
+package uk.co.epii.stephenson.cif;
+
+/**
+ * User: James Robinson
+ * Date: 17/08/2014
+ * Time: 14:17
+ */
+public interface NationalRailTime {
+
+  public int getAdditionalDays();
+  public int getHours();
+  public int getMinutes();
+  public int getSeconds();
+
+}
